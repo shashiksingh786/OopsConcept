e8c4af5f27f11b5915909ae93c45b07c828bd3b5
Shashi Kiran Singh <to.shashi7886@hotmail.com>
2023-07-06 18:44:22 +0530

    add abstract class and use in oops

---
+++ OopsConcept/Abstract.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    public abstract class AndriodBase
+    {
+        public string? Name { get; set; }
+        public double Price { get; set; }
+        public double DevelopmentCost { get; set; }
+        public double CompanyCost { get; set; }
+
+        public virtual void PriceBenchmark() { }
+    }
+
+    public class ChromeBook : AndriodBase
+    {
+
+    }
+    public class GooglePixelMobile : AndriodBase
+    {
+        public bool LightWeight { get; set; }
+
+        public override void PriceBenchmark()
+        {
+            Price = DevelopmentCost + CompanyCost;
+            Console.WriteLine("Price  : " + Price + Environment.NewLine);
+        }
+    }
+
+    public class GoogleBookTablet : AndriodBase
+    {
+        public double ResearchCost { get; set; }
+        public override void PriceBenchmark()
+        {
+            Price = DevelopmentCost + CompanyCost + ResearchCost;
+            Console.WriteLine("Price  : "+Price+Environment.NewLine);
+
+        }
+    }
+}
--- OopsConcept/Program.cs
+++ OopsConcept/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using OopsConcept;
+
 Console.WriteLine("Hello, World!");
+
+ChromeBook chromeBook = new()
+{
+    Name = "Chrome Book"
+};
+Console.WriteLine($"Name :  {chromeBook.Name}");
+
+
+GooglePixelMobile googlePixelMobile = new()
+{
+    Name = "Google Pixel",
+    Price = 30000,
+    CompanyCost = 15000,
+    DevelopmentCost = 7000,
+    LightWeight = true
+};
+
+Console.WriteLine($"Name : {googlePixelMobile.Name}");
+googlePixelMobile.PriceBenchmark();
+
+GoogleBookTablet googleBookTablet = new()
+{
+    Name = "Google Book",
+    Price = 45000,
+    CompanyCost = 17000,
+    DevelopmentCost = 9000,
+    ResearchCost = 5000
+};
+Console.WriteLine($"Name : {googleBookTablet.Name}");
+googleBookTablet.PriceBenchmark();
